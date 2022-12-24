231103e6c05fdc80b119ca56098a6cf6eb4f13e8
Milichihin <milichihin@gmail.com>
2022-12-25 01:14:45 +0200

    set gallery grid

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import '../Styles/gallery.css';
 import { images } from '../Data/images.js';
-import { arrow } from '../Data/images.js'; 
+import { arrow } from '../Data/images.js';
 import { closeButton } from '../Data/images.js';
 import { useState } from 'react';
 
@@ -21,7 +21,7 @@ const Gallery = () => {
         let i = data.i;
         if (action === 'next-img') {
             setData({ img: images[i + 1], i: [i + 1] });
-        } 
+        }
         if (action === 'prev-img') {
             setData({ img: images[i - 1], i: [i - 1] });
         }
@@ -37,18 +37,18 @@ const Gallery = () => {
                     data.img &&
                     <>
                         <div className='fullsize-wraper'>
-                            <div className='fullsize-background' onClick={()=> changeImage()}/>
-                            <img src={closeButton} className='cross' onClick={()=> changeImage()}/>
-                            <img src={arrow} className='previous-arrow'  onClick={()=> changeImage('prev-img')}/>
+                            <div className='fullsize-background' onClick={() => changeImage()} />
+                            <img src={closeButton} className='cross' onClick={() => changeImage()} />
+                            <img src={arrow} className='previous-arrow' onClick={() => changeImage('prev-img')} />
                             <img src={data.img} className='fullsize-img' />
-                            <img src={arrow} className='next-arrow'  onClick={()=> changeImage('next-img')}/>
+                            <img src={arrow} className='next-arrow' onClick={() => changeImage('next-img')} />
                         </div>
                     </>
 
                 }
-                
+
                 <ResponsiveMasonry
-                    columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+                    columnsCountBreakPoints={{ 300: 1, 500: 2, 700: 3 }}
                 >
                     <Masonry gutter='15px'>
                         {images.map((image, i) => (
