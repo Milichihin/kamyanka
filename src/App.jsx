8099276b89adf0543c395926a5be5cade7503b97
Milichihin <milichihin@gmail.com>
2022-12-25 23:46:02 +0200

    create a Greating component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
-import Logo from './Components/Logo/Logo.jsx'
-import Gallery from './Components/Gallery/Gallery.jsx'
-import Navigation from './Components/Navigation/Navigation.jsx'
-import Conception from './Components/Conception/Conception.jsx'
+import Logo from './Components/Logo/Logo.jsx';
+import Greating from './Components/Greating/Greating.jsx';
+import Gallery from './Components/Gallery/Gallery.jsx';
+import Navigation from './Components/Navigation/Navigation.jsx';
+import Conception from './Components/Conception/Conception.jsx';
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>Kamyanka</h1> */}
       <Logo/>
       <Navigation/>
+      <Greating/>
       <Gallery/>
       <Conception/>
     </div>
---
+++ src/Components/Greating/Greating.jsx
@@ -0,0 +1,16 @@
+import './greating.css';
+
+function Greating() {
+
+    return (
+        <>
+            <div className="greating">
+                Greating
+            </div>
+        </>
+
+
+    );
+}
+
+export default Greating;
