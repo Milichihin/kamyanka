8b858d65a1bf3c80bfd799987ac97c5dc48795a7
Milichihin <milichihin@gmail.com>
2022-12-30 03:39:01 +0200

    add Queue styles

--- src/Components/Building/QueuesBlock/QueuesBlock.jsx
+++ src/Components/Building/QueuesBlock/QueuesBlock.jsx
@@ -7,31 +7,40 @@ function QueuesBlock() {
 
     const [queue, setQueue] = useState(queuesArray[0]);
 
-    const chooseQueue = (queue) => {
-        setQueue(queue)
+    const chooseQueue = (item) => {
+        setQueue(item)
     }
 
     return (
         <>
-            <div>
-                {queuesArray.map(queue => (
-                    <div onClick={() => chooseQueue(queue)}>
-                        {queue.header}
+            <div className='queue-container'>
+                {queuesArray.map(item => (
+                    <div
+                        className={"select " + (item.header === queue.header ? "active-select" : "")}
+                        onClick={() => chooseQueue(item)}
+                    >
+                        {item.header}
                     </div>
                 ))}
             </div>
-            <div>
-                <p>{queue.deadline}</p>
-                <p>{queue.type}</p>
-                <p>{queue.number}</p>
-                <p>{queue.description}</p>
-                {
-                    queue.details &&
-                    queue.details.map((item) => (
-                        <p>{item}</p>
-                    ))
-                }
-                <img src={queue.photo}/>
+            <div className='queue-wraper'>
+                <div className='queue-description'>
+                    <p>{queue.deadline}</p>
+                    <p>{queue.type}</p>
+                    <p>{queue.number}</p>
+                    <p>{queue.description}</p>
+                    <div className='details'>
+                        {
+                            queue.details &&
+                            queue.details.map((item) => (
+                                <p>{item}</p>
+                            ))
+                        }
+                    </div>
+
+                </div>
+
+                <img src={queue.photo} />
                 {/* {
                     (Object.values(queue).splice(1)).map((value) => {
                         if(value.includes("http")){
