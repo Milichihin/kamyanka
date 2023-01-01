7c6c1b66723387aeb86b64553d5fb67c4687a23f
Milichihin <milichihin@gmail.com>
2023-01-01 20:01:50 +0200

    add contact

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,16 @@ import Advantages from './Components/Advantages/Advantages.jsx';
 import Contacts from './Components/Contacts/Contacts.jsx';
 
 function App() {
+
+
   return (
     <div className="App">
-      <Header navArray={navArray}/>
-      <Conception anchor={navArray.Greating}/>
+      <div id='qwer'>hello</div>
+      <Header navArray={navArray} />
+      <Conception anchor={navArray.Greating} />
       <Building anchor={navArray.Building} />
-      <Principle anchor={navArray.Principle}  />
-      <Advantages anchor={navArray.Advantages}  />
+      <Principle anchor={navArray.Principle} />
+      <Advantages anchor={navArray.Advantages} />
       <Contacts anchor={navArray.Contacts} />
     </div>
   );
