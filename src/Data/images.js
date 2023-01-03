bb5f55b984020a23520e5709d169d6784909aa5f
Milichihin <milichihin@gmail.com>
2023-01-03 19:13:15 +0200

    change source of images

--- src/Data/images.js
+++ src/Data/images.js
@@ -2,7 +2,10 @@
 // Gallery
 ////////////////////   localhost  ////////////////////
 
-const imagesInitial0 = [
+
+export const images = imagesInitial.sort((a, b) => 0.5 - Math.random()).slice(0, 10);
+
+const imagesInitial1 = [
     "http://localhost/img/1.jpg",
     "http://localhost/img/2.jpg",
     "http://localhost/img/3.jpg",
@@ -41,80 +44,62 @@ const imagesInitial0 = [
     "http://localhost/img/36.jpg",
     "http://localhost/img/37.jpg"
 ];
-
-const imagesInitial = [
-    "http://localhost/img/1.jpg",
-    "http://localhost/img/2.jpg",
-    "http://localhost/img/3.jpg",
-    "http://localhost/img/4.jpg",
-    "http://localhost/img/5.jpg",
-    "http://localhost/img/6.jpg",
-    "http://localhost/img/7.jpg",
-    "http://localhost/img/8.jpg",
-    "http://localhost/img/9.jpg",
-    "http://localhost/img/10.jpg",
-    // "http://localhost/img/11.jpg",
-    // "http://localhost/img/12.jpg",
-    "http://localhost/img/13.jpg",
-    // "http://localhost/img/14.jpg",
-    "http://localhost/img/15.jpg",
-    "http://localhost/img/16.jpg",
-    "http://localhost/img/17.jpg",
-    // "http://localhost/img/18.jpg",
-    "http://localhost/img/19.jpg",
-    "http://localhost/img/20.jpg",
-    // "http://localhost/img/21.jpg",
-    "http://localhost/img/22.jpg",
-    "http://localhost/img/23.jpg",
-    "http://localhost/img/24.jpg",
-    "http://localhost/img/25.jpg",
-    "http://localhost/img/26.jpg",
-    "http://localhost/img/27.jpg",
-    "http://localhost/img/28.jpg",
-    "http://localhost/img/29.jpg",
-    // "http://localhost/img/30.jpg",
-    "http://localhost/img/31.jpg",
-    "http://localhost/img/32.jpg",
-    // "http://localhost/img/33.jpg",
-    // "http://localhost/img/34.jpg",
-    // "http://localhost/img/35.jpg",
-    "http://localhost/img/36.jpg",
-    "http://localhost/img/37.jpg"
-];
-
-export const images = imagesInitial.sort((a, b) => 0.5 - Math.random()).slice(0, 12);
 
 export const closeButton = "http://localhost/img/circle-icon-cross.png";
 
 export const arrow = "http://localhost/img/arrow.png";
 
+export const logo = "http://localhost/img/logo_kamyanka.png";
+
+export const wallpaper = "http://localhost/img/greating-wallpaper2.jpg";
 
 
 ////////////////////   GoogleDisc  ////////////////////
 
-export const imagesLinks = [
-    "https://drive.google.com/uc?export=view&id=1mo1NQCjIHdEyeXS5obZf5DkGepQ2UEgP",
-    "https://drive.google.com/uc?export=view&id=16U-mTA5JDJhBLYP7hlQSNFB-MMBUKV5u",
-    "https://drive.google.com/uc?export=view&id=1s4Bfq4doEd6A6iXeOtL0hRsZ-WJABKv0",
-    "https://drive.google.com/uc?export=view&id=1ptyLlgtnojJQDEntd-may3EffzjvyhCE",
-    "https://drive.google.com/uc?export=view&id=1WNDMklIcSzPl7ksvwCZZB44G5CZcOyr9",
-    "https://drive.google.com/uc?export=view&id=140yJCkoYnjYFGRxV4oPRbgIKGtfPHkd_",
-    "https://drive.google.com/uc?export=view&id=1VThEEVuS-UTEDPri5hmtwGJdWqLvitts",
-    "https://drive.google.com/uc?export=view&id=1SueuCbfDetyoXgbha6N0aZXFNPiF7a76",
-    "https://drive.google.com/uc?export=view&id=14c6ML-U_Om66h4cETIoHcrJRnXkobSMm",
-    "https://drive.google.com/uc?export=view&id=1tai4baYSvA1AKIX5Sd665R4aln4g4K5u",
-    "https://drive.google.com/uc?export=view&id=1tAQZypXr1EvwdcdUOqZ8GNB4jP3nQjAA",
-    "https://drive.google.com/uc?export=view&id=1bQJJ-cUd0fdHNXTRjvRwFD57m4eljKPC"
+export const imagesInitial = [
+    "https://drive.google.com/uc?export=view&id=1mo1NQCjIHdEyeXS5obZf5DkGepQ2UEgP", //1
+    "https://drive.google.com/uc?export=view&id=16U-mTA5JDJhBLYP7hlQSNFB-MMBUKV5u", //2
+    "https://drive.google.com/uc?export=view&id=1s4Bfq4doEd6A6iXeOtL0hRsZ-WJABKv0", //3
+    "https://drive.google.com/uc?export=view&id=1ptyLlgtnojJQDEntd-may3EffzjvyhCE", //4
+    "https://drive.google.com/uc?export=view&id=1WNDMklIcSzPl7ksvwCZZB44G5CZcOyr9", //5
+    "https://drive.google.com/uc?export=view&id=140yJCkoYnjYFGRxV4oPRbgIKGtfPHkd_", //6
+    "https://drive.google.com/uc?export=view&id=1VThEEVuS-UTEDPri5hmtwGJdWqLvitts", //7
+    "https://drive.google.com/uc?export=view&id=1SueuCbfDetyoXgbha6N0aZXFNPiF7a76", //8
+    "https://drive.google.com/uc?export=view&id=14c6ML-U_Om66h4cETIoHcrJRnXkobSMm", //9
+    "https://drive.google.com/uc?export=view&id=1tai4baYSvA1AKIX5Sd665R4aln4g4K5u", //10
+    "https://drive.google.com/uc?export=view&id=1tAQZypXr1EvwdcdUOqZ8GNB4jP3nQjAA", //11
+    "https://drive.google.com/uc?export=view&id=1bQJJ-cUd0fdHNXTRjvRwFD57m4eljKPC", //12
+    // "", //13
+    // "", //14
+    // "", //15
+    // "", //16
+    // "", //17
+    // "", //18
+    // "", //19
+    // "", //20
+    // "", //21
+    // "", //22
+    // "", //23
+    // "", //24
+    // "", //25
+    // "", //26
+    // "", //27
+    // "", //28
+    // "", //29
+    // "", //30
+    // "", //31
+    // "", //32
+    // "", //33
+    // "", //34
+    // "", //35
+    // "", //36
+    // "", //37
 ];
 
 export const closeButtonLink = "https://drive.google.com/uc?export=view&id=12Zq0dnUzXz2tMG1_ZG9jelP66p__qBfE";
 
-////////////////////   Logo  ////////////////////
-export const logo = "http://localhost/img/logo_kamyanka.png";
+export const arrowLink = "http://localhost/img/arrow.png";
 
-////////////////////   Greating  ////////////////////
-export const wallpaper = "http://localhost/img/greating-wallpaper2.jpg";
+export const logoLink = "http://localhost/img/logo_kamyanka.png";
 
-
-
-
+export const wallpaperLink = "http://localhost/img/greating-wallpaper2.jpg";
