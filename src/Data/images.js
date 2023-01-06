47962e50b01cafb98cb14f92d363ed899295b6a9
Milichihin <milichihin@gmail.com>
2023-01-06 23:47:35 +0200

    optimized picture size

--- src/Data/images.js
+++ src/Data/images.js
@@ -3,7 +3,7 @@
 ////////////////////   localhost  ////////////////////
 
 
-const imagesInitial = [
+const imagesInitial0 = [
     "./images/1.webp",
     "./images/2.webp",
     "./images/3.webp",
@@ -43,6 +43,85 @@ const imagesInitial = [
     "./images/37.webp"
 ];
 
+const imagesInitial = [
+    "./images/original/1.webp",
+    "./images/original/2.webp",
+    "./images/original/3.webp",
+    "./images/original/4.webp",
+    "./images/original/5.webp",
+    "./images/original/6.webp",
+    "./images/original/7.webp",
+    "./images/original/8.webp",
+    "./images/original/9.webp",
+    "./images/original/10.webp",
+    // "./images/original/11.webp",
+    // "./images/original/12.webp",
+    "./images/original/13.webp",
+    // "./images/original/14.webp",
+    "./images/original/15.webp",
+    "./images/original/16.webp",
+    "./images/original/17.webp",
+    // "./images/original/18.webp",
+    "./images/original/19.webp",
+    "./images/original/20.webp",
+    // "./images/original/21.webp",
+    "./images/original/22.webp",
+    "./images/original/23.webp",
+    "./images/original/24.webp",
+    "./images/original/25.webp",
+    "./images/original/26.webp",
+    "./images/original/27.webp",
+    "./images/original/28.webp",
+    "./images/original/29.webp",
+    // "./images/original/30.webp",
+    "./images/original/31.webp",
+    "./images/original/32.webp",
+    // "./images/original/33.webp",
+    // "./images/original/34.webp",
+    // "./images/original/35.webp",
+    "./images/original/36.webp",
+    "./images/original/37.webp"
+];
+
+const imagesInitial700w = [
+    "./images/700w/1.webp",
+    "./images/700w/2.webp",
+    "./images/700w/3.webp",
+    "./images/700w/4.webp",
+    "./images/700w/5.webp",
+    "./images/700w/6.webp",
+    "./images/700w/7.webp",
+    "./images/700w/8.webp",
+    "./images/700w/9.webp",
+    "./images/700w/10.webp",
+    // "./images/700w/11.webp",
+    // "./images/700w/12.webp",
+    "./images/700w/13.webp",
+    // "./images/700w/14.webp",
+    "./images/700w/15.webp",
+    "./images/700w/16.webp",
+    "./images/700w/17.webp",
+    // "./images/700w/18.webp",
+    "./images/700w/19.webp",
+    "./images/700w/20.webp",
+    // "./images/700w/21.webp",
+    "./images/700w/22.webp",
+    "./images/700w/23.webp",
+    "./images/700w/24.webp",
+    "./images/700w/25.webp",
+    "./images/700w/26.webp",
+    "./images/700w/27.webp",
+    "./images/700w/28.webp",
+    "./images/700w/29.webp",
+    // "./images/700w/30.webp",
+    "./images/700w/31.webp",
+    "./images/700w/32.webp",
+    // "./images/700w/33.webp",
+    // "./images/700w/34.webp",
+    // "./images/700w/35.webp",
+    "./images/700w/36.webp",
+    "./images/700w/37.webp"
+];
 
 export const closeButton = "./images/circle-icon-cross.png";
 
@@ -50,12 +129,17 @@ export const arrow = "./images/arrow.png";
 
 export const logo = "./images/logo_kamyanka.png";
 
-export const wallpaper = "./images/greating-wallpaper2.webp";
+export const wallpaper = ((document.documentElement.scrollWidth >= 700)
+    ? "./images/original/greating-wallpaper2.webp"
+    : "./images/700w/greating-wallpaper2.webp");
 
+
+    const pageWidth = document.documentElement.scrollWidth;
+    console.log("pageWidth = images.js" + pageWidth);
 
 ////////////////////   GoogleDisc  ////////////////////
 
-const imagesInitial0 = [
+const imagesInitial1 = [
     "https://drive.google.com/uc?export=view&id=1mo1NQCjIHdEyeXS5obZf5DkGepQ2UEgP",
     "https://drive.google.com/uc?export=view&id=16U-mTA5JDJhBLYP7hlQSNFB-MMBUKV5u",
     "https://drive.google.com/uc?export=view&id=1s4Bfq4doEd6A6iXeOtL0hRsZ-WJABKv0",
@@ -108,4 +192,8 @@ const imagesInitial0 = [
 
 
 
-export const images = imagesInitial.sort((a, b) => 0.5 - Math.random()).slice(0, 10);
+export const images = ((document.documentElement.scrollWidth >= 700)
+    ? imagesInitial
+    : imagesInitial700w)
+    .sort((a, b) => 0.5 - Math.random())
+    .slice(0, 12);
