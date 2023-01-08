89a7a5c7bca7185d9fb829b68b0864ce9c895b92
Milichihin <milichihin@gmail.com>
2023-01-09 01:10:57 +0200

    add burger menu

--- src/Components/Header/Navigation/Navigation.jsx
+++ src/Components/Header/Navigation/Navigation.jsx
@@ -1,35 +1,66 @@
 import './navigation.css';
+import './burgernav.css';
 import { useState } from 'react';
 import Logo from '../Logo/Logo.jsx';
 
 function Navigation(props) {
 
     const [active, setActive] = useState(props.navArray.Greating);
+    const [showBurger, setShowBurger] = useState(false);
 
     const activateItem = (menuItem) => {
         setActive(menuItem);
+        setBurger();
     }
-    // console.log(props.fixNav);
+
+    const setBurger = () => {
+        setShowBurger(!showBurger);
+    }
 
     return (
         <>
-            <Logo fixNav={props.showFixNav} />
-            <div className="nav-container">
-                {
-                    Object.values(props.navArray).map((menuItem, i) => (
-                        <a
-                            href={"#" + menuItem}
-                            className={(menuItem === active ? "active" : "item")}
-                            onClick={() => activateItem(menuItem)}
-                        >
-                            {menuItem}
-                        </a>
-                    ))
-                }
+            <div className='desktop-menu'>
+                <Logo fixNav={props.showFixNav} />
+                <div className="nav-container">
+                    {
+                        Object.values(props.navArray).map((menuItem, i) => (
+                            <a
+                                href={"#" + menuItem}
+                                className={(menuItem === active ? "active" : "item")}
+                                onClick={() => activateItem(menuItem)}
+                            >
+                                {menuItem}
+                            </a>
+                        ))
+                    }
+                </div>
             </div>
-            <div className='fix-wrap' style={props.fixNav?{top: 0, opacity: 1}:{top: -50, opacity: 0}} >
+            <div className='desktop-menu-fix' >
+                <div className='fix-wrap' style={props.fixNav ? { top: 0, opacity: 1 } : { top: -50, opacity: 0 }} >
+                    <div className="nav-container">
+                        <Logo fixNav={props.fixNav} />
+                        {
+                            Object.values(props.navArray).map((menuItem, i) => (
+                                <a
+                                    href={"#" + menuItem}
+                                    className={(menuItem === active ? "active" : "item")}
+                                    onClick={() => activateItem(menuItem)}
+                                >
+                                    {menuItem}
+                                </a>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className='mobile-menu'>
+                <div className='fix-wrap'>
+                    <Logo fixNav={props.fixNav} />
+                    <div className='burger-icon' onClick={() => setBurger()} >&#9776;</div>
+
+                    {
+                        showBurger &&
                         <div className="nav-container">
-                            <Logo fixNav={props.fixNav} />
                             {
                                 Object.values(props.navArray).map((menuItem, i) => (
                                     <a
@@ -42,30 +73,10 @@ function Navigation(props) {
                                 ))
                             }
                         </div>
-                    </div>
+                    }
 
-            {/* {
-                props.fixNav &&
-                <>
-                    <div className='fix-wrap' style={{top: 0}} >
-                        <div className="nav-container">
-                            <Logo fixNav={props.fixNav} />
-                            {
-                                Object.values(props.navArray).map((menuItem, i) => (
-                                    <a
-                                        href={"#" + menuItem}
-                                        className={(menuItem === active ? "active" : "item")}
-                                        onClick={() => activateItem(menuItem)}
-                                    >
-                                        {menuItem}
-                                    </a>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </>
-
-            } */}
+                </div>
+            </div>
         </>
     );
 }
