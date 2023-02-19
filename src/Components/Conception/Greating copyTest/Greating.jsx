94eddbb5f036ff98dd0528ed94b46869aa5bae07
Milichihin <milichihin@gmail.com>
2023-02-19 13:52:33 +0200

    test comit

---
+++ src/Components/Conception/Greating copyTest/Greating.jsx
@@ -0,0 +1,31 @@
+import './greating.css';
+import { wallpaper } from '../../../Data/images.js';
+
+function Greating(props) {
+
+    const greating = props.greating;
+
+    return (
+        <div>
+            <a className='anchor-padding' name={props.anchor} ></a>
+            <div className="wallpaper-wraper">
+                <img src={wallpaper} />
+                
+                <div className="gradient"></div>
+                <div className="greating-slogan">{greating.slogan}</div>
+                <h2>
+                    {greating.header}
+                </h2>
+            </div>
+            <div 
+                className="section-block"
+            >
+                <p style={{ marginTop: '40px' }}>
+                    <span style={{ fontWeight: '700', fontSize: "1.1em"}}>{greating.boldText}</span>{greating.text}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Greating;
